c0671e2e63b3091a0c15f6452361deab3760570e
Jaroslav Havlin <380698+jhavlin@users.noreply.github>
2021-02-09 21:56:03 +0100

    Use relative paths

---
+++ elmapp.config.js
@@ -0,0 +1,8 @@
+/*
+More config information here:
+https://github.com/halfzebra/create-elm-app/blob/master/template/README.md#overriding-webpack-config
+*/
+
+module.exports = {
+    homepage: "./" //required to normalise path
+}
